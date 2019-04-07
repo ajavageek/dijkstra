68aa2bffb9197635431984c0b3285ac8b94f73f3
Nicolas Frankel <nicolas@frankel.ch>
2019-04-07 19:39:51 +0200

    Replace loop with recursivity

--- kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
+++ kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
@@ -5,22 +5,23 @@ class Graph(private val weightedPaths: Map<String, Map<String, Int>>) {
     private val evaluatedPaths = mutableMapOf<String, Int>()
 
     fun findShortestPath(start: String, end: String): Int {
-        var current = start
-        while (current != end) {
-            current = updateShortestPath(current)
-        }
+        recurseFindShortestPath(start, end)
         return evaluatedPaths.getValue(end)
     }
 
-    private fun updateShortestPath(node: String): String {
-        weightedPaths[node]?.forEach {
-            val currentDistance = evaluatedPaths.getOrDefault(it.key, Integer.MAX_VALUE)
-            val newDistance = it.value + evaluatedPaths.getOrDefault(node, 0)
-            if (newDistance < currentDistance)
-                evaluatedPaths[it.key] = newDistance
+    private tailrec fun recurseFindShortestPath(node: String, end: String): String {
+        return if (node == end) end
+        else {
+            weightedPaths[node]?.forEach {
+                val currentDistance = evaluatedPaths.getOrDefault(it.key, Integer.MAX_VALUE)
+                val newDistance = it.value + evaluatedPaths.getOrDefault(node, 0)
+                if (newDistance < currentDistance)
+                    evaluatedPaths[it.key] = newDistance
+            }
+            evaluatedPaths.remove(node)
+            val nextNode = evaluatedPaths.minBy { it.value }?.key
+                ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
+            recurseFindShortestPath(nextNode, end)
         }
-        evaluatedPaths.remove(node)
-        return evaluatedPaths.minBy { it.value }?.key
-            ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
     }
 }
