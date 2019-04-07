7fc9189eb4884438d66bf73f13c7880da777c1b7
Nicolas Frankel <nicolas@frankel.ch>
2019-04-07 23:21:55 +0200

    Finishing touches

--- kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
+++ kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
@@ -3,28 +3,28 @@ package ch.frankel.blog.dijkstra
 class Graph(private val weightedPaths: Map<String, Map<String, Int>>) {
 
     fun findShortestPath(start: String, end: String): Int {
-        val (_, paths) = recurseFindShortestPath(NodePaths(start), end)
+        val paths = recurseFindShortestPath(NodePaths(start, end)).paths
         return paths.getValue(end)
     }
 
-    private tailrec fun recurseFindShortestPath(nodePaths: NodePaths, end: String): NodePaths {
-        return if (nodePaths.node == end) NodePaths(end, nodePaths.paths)
+    private tailrec fun recurseFindShortestPath(nodePaths: NodePaths): NodePaths {
+        return if (nodePaths.isFinished()) nodePaths
         else {
             val nextNode = nodePaths.nextNode(weightedPaths)
-            recurseFindShortestPath(nextNode, end)
+            recurseFindShortestPath(nextNode)
         }
     }
 }
 
-data class NodePaths(val node: String, val paths: Map<String, Int> = emptyMap()) {
+data class NodePaths(private val node: String, private  val end: String, val paths: Map<String, Int> = emptyMap()) {
 
     private fun updatePath(entry: Map.Entry<String, Int>): NodePaths {
         val currentDistance = paths.getOrDefault(entry.key, Integer.MAX_VALUE)
         val newDistance = entry.value + paths.getOrDefault(node, 0)
         return if (newDistance < currentDistance)
-            copy(paths = paths + (entry.key to newDistance))
+            this + (entry.key to newDistance)
         else
-            copy(paths = paths)
+            this
     }
 
     private fun updatePaths(weightedPaths: Map<String, Map<String, Int>>) = (weightedPaths[node]
@@ -33,17 +33,15 @@ data class NodePaths(val node: String, val paths: Map<String, Int> = emptyMap())
 
     fun nextNode(weightedPaths: Map<String, Map<String, Int>>): NodePaths {
         val updatedPaths = updatePaths(weightedPaths)
-        val nextNode = updatedPaths.paths.minBy { it.value }?.key
-            ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
+        val nextNode = updatedPaths.paths.minBy { it.value }?.key as String
         return updatedPaths.copy(node = nextNode)
     }
 
-    operator fun plus(other: NodePaths): NodePaths {
-        if (node != other.node) throw RuntimeException("Not possible to merge NodePaths with different nodes")
-        return copy(paths = paths + other.paths)
-    }
+    fun isFinished() = node == end
 
-    operator fun minus(node: String): NodePaths {
-        return copy(paths = paths - node)
-    }
+    operator fun plus(other: NodePaths) = copy(paths = paths + other.paths)
+
+    operator fun plus(pair: Pair<String, Int>) = copy(paths = paths + pair)
+
+    operator fun minus(node: String)= copy(paths = paths - node)
 }
--- kotlin/src/test/kotlin/ch/frankel/blog/dijkstra/GraphTest.kt
+++ kotlin/src/test/kotlin/ch/frankel/blog/dijkstra/GraphTest.kt
@@ -1,45 +1,49 @@
 package ch.frankel.blog.dijkstra
 
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 import strikt.api.expect
 import strikt.assertions.isEqualTo
 
 class GraphTest {
 
-    @Test
-    fun `should return 20 for simple graph on Wikipedia`() {
+    @DataProvider
+    fun data(): Array<Array<Any>> {
         // https://fr.wikipedia.org/wiki/Probl%C3%A8me_de_plus_court_chemin
-        val nodes = mapOf<String, Map<String, Int>>(
-            "A" to mapOf("B" to 4, "C" to 2),
-            "B" to mapOf("C" to 5, "D" to 10),
-            "C" to mapOf("E" to 3),
-            "D" to mapOf("F" to 11),
-            "E" to mapOf("D" to 4)
+        val graph1 = Graph(
+            mapOf(
+                "A" to mapOf("B" to 4, "C" to 2),
+                "B" to mapOf("C" to 5, "D" to 10),
+                "C" to mapOf("E" to 3),
+                "D" to mapOf("F" to 11),
+                "E" to mapOf("D" to 4)
+            )
         )
-        val graph = Graph(nodes)
-        expect {
-            that(graph.findShortestPath("A", "F")).isEqualTo(20)
-        }
+        // https://fr.wikipedia.org/wiki/Algorithme_de_Dijkstra
+        val graph2 = Graph(
+            mapOf(
+                "A" to mapOf("B" to 85, "C" to 217, "E" to 173),
+                "B" to mapOf("A" to 85, "F" to 80),
+                "C" to mapOf("A" to 217, "G" to 186, "H" to 103),
+                "D" to mapOf("H" to 183),
+                "E" to mapOf("A" to 173, "J" to 502),
+                "F" to mapOf("B" to 80, "I" to 250),
+                "G" to mapOf("C" to 186),
+                "H" to mapOf("C" to 103, "D" to 183, "J" to 167),
+                "I" to mapOf("F" to 250, "J" to 84),
+                "J" to mapOf("I" to 84, "H" to 167, "E" to 502)
+            )
+        )
+        return arrayOf(
+            arrayOf(graph1, "A", "J", 487),
+            arrayOf(graph2, "A", "F", 20)
+        )
     }
 
-    @Test
-    fun `should return 487 for directed graph on Wikipedia`() {
-        // https://fr.wikipedia.org/wiki/Algorithme_de_Dijkstra
-        val nodes = mapOf<String, Map<String, Int>>(
-            "A" to mapOf("B" to 85, "C" to 217, "E" to 173),
-            "B" to mapOf("A" to 85, "F" to 80),
-            "C" to mapOf("A" to 217, "G" to 186, "H" to 103),
-            "D" to mapOf("H" to 183),
-            "E" to mapOf("A" to 173, "J" to 502),
-            "F" to mapOf("B" to 80, "I" to 250),
-            "G" to mapOf("C" to 186),
-            "H" to mapOf("C" to 103, "D" to 183, "J" to 167),
-            "I" to mapOf("F" to 250, "J" to 84),
-            "J" to mapOf("I" to 84, "H" to 167, "E" to 502)
-        )
-        val graph = Graph(nodes)
+    @Test(dataProvider = "data")
+    fun `should match shortest path`(graph: Graph, start: String, end: String, expectedLength: Int) {
         expect {
-            that(graph.findShortestPath("A", "J")).isEqualTo(487)
+            that(graph.findShortestPath(start, end)).isEqualTo(expectedLength)
         }
     }
 }
