8baaed3cb0a96cb2560932003e6b0c25be31568e
Nicolas Frankel <nicolas@frankel.ch>
2019-04-07 21:07:17 +0200

    Constraint mutability to one single function signature

--- kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
+++ kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
@@ -3,15 +3,15 @@ package ch.frankel.blog.dijkstra
 class Graph(private val weightedPaths: Map<String, Map<String, Int>>) {
 
     fun findShortestPath(start: String, end: String): Int {
-        val (_, paths) = recurseFindShortestPath(start, end, mutableMapOf())
+        val (_, paths) = recurseFindShortestPath(start, end, mapOf())
         return paths.getValue(end)
     }
 
-    private tailrec fun recurseFindShortestPath(node: String, end: String, paths: MutableMap<String, Int>): Pair<String, MutableMap<String, Int>> {
+    private tailrec fun recurseFindShortestPath(node: String, end: String, paths: Map<String, Int>): Pair<String, Map<String, Int>> {
         return if (node == end) end to paths
         else {
-            val updatedPaths = updatePaths(node, paths)
-            val nextNode = paths.minBy { it.value }?.key
+            val updatedPaths = updatePaths(node, paths.toMutableMap())
+            val nextNode = updatedPaths.minBy { it.value }?.key
                 ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
             recurseFindShortestPath(nextNode, end, updatedPaths)
         }
