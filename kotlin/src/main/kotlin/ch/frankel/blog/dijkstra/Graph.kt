3e987efde266ef7b5d87492c41088cafac5eeff8
Nicolas Frankel <nicolas@frankel.ch>
2019-04-07 19:41:50 +0200

    Move class state to accumulator parameter

--- kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
+++ kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
@@ -2,26 +2,25 @@ package ch.frankel.blog.dijkstra
 
 class Graph(private val weightedPaths: Map<String, Map<String, Int>>) {
 
-    private val evaluatedPaths = mutableMapOf<String, Int>()
-
     fun findShortestPath(start: String, end: String): Int {
-        recurseFindShortestPath(start, end)
-        return evaluatedPaths.getValue(end)
+        val paths = mutableMapOf<String, Int>()
+        recurseFindShortestPath(start, end, paths)
+        return paths.getValue(end)
     }
 
-    private tailrec fun recurseFindShortestPath(node: String, end: String): String {
+    private tailrec fun recurseFindShortestPath(node: String, end: String, paths: MutableMap<String, Int>): String {
         return if (node == end) end
         else {
             weightedPaths[node]?.forEach {
-                val currentDistance = evaluatedPaths.getOrDefault(it.key, Integer.MAX_VALUE)
-                val newDistance = it.value + evaluatedPaths.getOrDefault(node, 0)
+                val currentDistance = paths.getOrDefault(it.key, Integer.MAX_VALUE)
+                val newDistance = it.value + paths.getOrDefault(node, 0)
                 if (newDistance < currentDistance)
-                    evaluatedPaths[it.key] = newDistance
+                    paths[it.key] = newDistance
             }
-            evaluatedPaths.remove(node)
-            val nextNode = evaluatedPaths.minBy { it.value }?.key
+            paths.remove(node)
+            val nextNode = paths.minBy { it.value }?.key
                 ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
-            recurseFindShortestPath(nextNode, end)
+            recurseFindShortestPath(nextNode, end, paths)
         }
     }
 }
