61931032cc7c784a2fc2ea6b64f38e195394962c
Nicolas Frankel <nicolas@frankel.ch>
2019-04-07 22:43:26 +0200

    Finish moving behavior to data class

--- kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
+++ kotlin/src/main/kotlin/ch/frankel/blog/dijkstra/Graph.kt
@@ -10,10 +10,8 @@ class Graph(private val weightedPaths: Map<String, Map<String, Int>>) {
     private tailrec fun recurseFindShortestPath(nodePaths: NodePaths, end: String): NodePaths {
         return if (nodePaths.node == end) NodePaths(end, nodePaths.paths)
         else {
-            val updatedPaths = nodePaths.updatePaths(weightedPaths)
-            val nextNode = updatedPaths.paths.minBy { it.value }?.key
-                ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
-            recurseFindShortestPath(NodePaths(nextNode, updatedPaths.paths), end)
+            val nextNode = nodePaths.nextNode(weightedPaths)
+            recurseFindShortestPath(nextNode, end)
         }
     }
 }
@@ -29,9 +27,16 @@ data class NodePaths(val node: String, val paths: Map<String, Int> = emptyMap())
             copy(paths = paths)
     }
 
-    fun updatePaths(weightedPaths: Map<String, Map<String, Int>>) = (weightedPaths[node]
+    private fun updatePaths(weightedPaths: Map<String, Map<String, Int>>) = (weightedPaths[node]
         ?.map { updatePath(it) }
         ?.fold(copy()) { acc, item -> acc + item } ?: copy()) - node
+
+    fun nextNode(weightedPaths: Map<String, Map<String, Int>>): NodePaths {
+        val updatedPaths = updatePaths(weightedPaths)
+        val nextNode = updatedPaths.paths.minBy { it.value }?.key
+            ?: throw RuntimeException("Map was empty, this cannot happen with a non-empty graph")
+        return updatedPaths.copy(node = nextNode)
+    }
 
     operator fun plus(other: NodePaths): NodePaths {
         if (node != other.node) throw RuntimeException("Not possible to merge NodePaths with different nodes")
